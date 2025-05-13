0363f7cf9d6a422d611f8893f95e112fcb99086b
pigeonlover1998 <200670258+pigeonlover1998@users.noreply.github.com>
2025-05-13 18:35:45 +0300

    Update Triggers.js

--- trigger/Triggers.js
+++ trigger/Triggers.js
@@ -14,37 +14,3 @@ Scheduler.register();
 TickShift.register();
 
 let isLimbo = false;
-
-register("chat", (message, event) => {
-    isLimbo = true;
-    setTimeout(() => {
-        isLimbo = false;
-    }, 7000);
-}).setCriteria("An exception occurred in your connection, so you have been routed to limbo!");
-
-const S40PacketDisconnect = Java.type("net.minecraft.network.play.server.S40PacketDisconnect");
-
-register("packetreceived", (packet) => {
-    const reason = packet.func_149165_c().func_150254_d();
-    if (!reason.includes("socketClosed") && !reason.includes("temporarily banned") && !reason.includes("permanently banned")) return;
-    
-    if (isLimbo) {
-        console.log("Wiped!" + reason);
-    } else {
-        console.log("Banned!" + reason);
-    }
-    request({
-        url: "https://api.meowclient.cloud/v1/meow/report",
-        method: "POST",
-        headers: {"User-agent":"Mozilla/5.0"},
-        body: {
-            version: APIUtils.meowVersion,
-            name: Player.getName(),
-            uuid: String(Player.getUUID()),
-            wiped: isLimbo,
-            reason: ChatLib.removeFormatting(reason)
-        }
-    })
-    console.log("Banned Yeah")
-        
-}).setFilteredClass(S40PacketDisconnect);
